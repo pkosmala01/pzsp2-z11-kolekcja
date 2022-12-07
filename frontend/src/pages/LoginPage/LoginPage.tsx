5929e7ef1780f8c862f6f7dca7dc921ba724d2a3
Truong Giang Do <giangh14cqt@gmail.com>
2022-12-07 02:54:42 +0100

    Add type

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import {
@@ -10,11 +11,25 @@ import {
 } from "./LoginPage.styles";
 
 const LoginPage = () => {
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
   const navigate = useNavigate();
 
   const loginHandler = () => {
     console.log("login");
+    console.log(username);
+    console.log(password);
     navigate("/");
+  };
+
+  const handleUsernameChange = (e: any) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
   };
 
   return (
@@ -22,8 +37,8 @@ const LoginPage = () => {
       <LoginBox>
         <Title>LOGIN</Title>
         <MyFormControl>
-          <TextField id="filled-username" label="Username" variant="filled" />
-          <TextField id="filled-password" label="Password" variant="filled" />
+          <TextField id="filled-username" onChange={handleUsernameChange} label="Username" variant="filled" required/>
+          <TextField id="filled-password" onChange={handlePasswordChange} label="Password" variant="filled" required type="password"/>
         </MyFormControl>
         <Button onClick={loginHandler} variant="contained" sx={LoginButton}>
           LOGIN
