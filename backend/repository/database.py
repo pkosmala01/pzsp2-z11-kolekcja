e88d8231c1bc3f33f806b13d7f0a4903884f01e0
Piotr Szmurlo <pszmurlo@box.com>
2022-12-09 10:56:56 +0100

    fix for locking out database

--- backend/repository/database.py
+++ backend/repository/database.py
@@ -19,6 +19,8 @@ def get_engine():
     with open("config.yaml") as config_file:
         config = yaml.safe_load(config_file)['database']
         db_password = os.getenv('DB_PASSWORD')
+        if db_password is None:
+            raise Exception("DB password not set")
         connection_uri = f"{config['dialect']}+{config['driver']}://{config['username']}:{db_password}@{config['host']}:{config['port']}/{config['dbname']}"
         return create_engine(connection_uri.format(db_password=db_password))
 
