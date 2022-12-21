f5425236bdf8645f19d8e821a0fcab7d8be15f4c
Truong Giang Do <giangh14cqt@gmail.com>
2022-12-21 19:15:35 +0100

    Navigate to item details

--- frontend/src/components/ItemsGrid/ItemsGrid.tsx
+++ frontend/src/components/ItemsGrid/ItemsGrid.tsx
@@ -9,6 +9,7 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const ItemsGrid = (props: { param: string | undefined }) => {
   //   const { data, isLoading } = useGetData(`collections/${props.param}/items`);
@@ -36,25 +37,29 @@ const ItemsGrid = (props: { param: string | undefined }) => {
         {items &&
           items.map((e: any) => {
             return (
-              <Grid item xs={5} md={3} key={e.item_id}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={e.image}
-                      alt={e.name}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {e.name}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {e.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+              <Grid item xs={5} md={3}>
+                <Link
+                  to={`/collection/${props.param}/items/${e.item_id}`}
+                  key={e.item_id}>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={e.image}
+                        alt={e.name}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {e.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {e.description}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </Link>
               </Grid>
             );
           })}
